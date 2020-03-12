73a5e335053e9ea1ada92751fc920ebfed68064c
Bartosz Rudnik <rudnik49@gmail.com>
2020-03-12 12:19:27 +0100

    tablica dynamiczna- podstawowe funkcje

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Tablica tablica = new Tablica();
+
+        tablica.dodajKoniec(12);
+        tablica.dodajKoniec(13);
+        tablica.usunKoniec();
+        tablica.dodajKoniec(15);
+        tablica.usunKoniec();
+        tablica.dodajKoniec(2);
+        tablica.wypiszTablice();
+
+    }
+
+}
--- src/Tablica.java
+++ src/Tablica.java
@@ -4,6 +4,78 @@ public class Tablica {
     private int pojemnosc;
     private int index;
 
+    public Tablica() {
 
+        tablica = new int[10];
+        index = 0;
+        pojemnosc = 10;
+
+    }
+
+    public void dodajKoniec(int liczba) {
+
+        if (index >= pojemnosc) {
+            powiekszTablice();
+        }
+
+        tablica[index] = liczba;
+        index++;
+
+    }
+
+    public void usunKoniec() {
+
+        if (index > 0) {
+            tablica[index] = 0;
+            index--;
+        }
+
+    }
+
+    private void powiekszTablice() {
+
+        int pomocnicza[] = new int[pojemnosc * 2];
+
+        for (int i = 0; i < index; i++) {
+
+            pomocnicza[i] = tablica[i];
+
+        }
+
+        pojemnosc *= 2;
+        tablica = pomocnicza;
+
+    }
+
+    private void zmniejszTablice() {
+
+        int pomocnicza[] = null;
+
+        if (index > 0) {
+
+            pomocnicza = new int[index];
+
+            for (int i = 0; i < index; i++) {
+
+                pomocnicza[i] = tablica[i];
+
+            }
+
+            pojemnosc = index;
+            tablica = pomocnicza;
+
+        }
+
+    }
+
+    public void wypiszTablice() {
+
+        for (int i = 0; i < index; i++) {
+
+            System.out.println(tablica[i]);
+
+        }
+
+    }
 
 }
