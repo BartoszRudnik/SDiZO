2a26dd46b42b7ffbd35a146d7b165b947cd39d36
Bartosz Rudnik <rudnik49@gmail.com>
2020-03-13 22:06:34 +0100

    kopiec -> wstepna implementacja

---
+++ src/Kopiec.java
@@ -0,0 +1,97 @@
+public class Kopiec {
+
+    private int[] kopiec;
+    private int index;
+    private int rozmiar;
+
+    public Kopiec() {
+
+        rozmiar = 100;
+        index = 1;
+        kopiec = new int[rozmiar];
+
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public void setRozmiar(int rozmiar) {
+        this.rozmiar = rozmiar;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getRozmiar() {
+        return rozmiar;
+    }
+
+    private void zwiekszIndex() {
+        index++;
+    }
+
+    private int indexRodzic(int pozycja) {
+        return (pozycja / 2);
+    }
+
+    private int indexLDziecko(int pozycja) {
+        return (2 * pozycja);
+    }
+
+    private int indexPDziecko(int pozycja) {
+        return (2 * pozycja + 1);
+    }
+
+    private void kopcuj(int pozycja) {
+
+        int l = indexLDziecko(pozycja);
+        int p = indexPDziecko(pozycja);
+        int max;
+
+        if (l <= getRozmiar() && kopiec[l] > kopiec[pozycja]) {
+            max = l;
+        } else
+            max = pozycja;
+
+        if (p <= getRozmiar() && kopiec[p] > kopiec[max]) {
+            max = p;
+        }
+
+        if (max != pozycja) {
+            int pomoc;
+            pomoc = kopiec[pozycja];
+            kopiec[pozycja] = kopiec[max];
+            kopiec[max] = pomoc;
+            kopcuj(max);
+        }
+
+    }
+
+    private void budujKopiec() {
+
+        setRozmiar(getIndex());
+        for (int i = getRozmiar() / 2; i >= 1; i--) {
+            kopcuj(i);
+        }
+
+    }
+
+    public void dodajKopiec(int liczba) {
+
+        kopiec[index] = liczba;
+        zwiekszIndex();
+        budujKopiec();
+
+    }
+
+    public void wyswietlKopiec() {
+
+        for (int i = 1; i < index; i++) {
+            System.out.println(kopiec[i]);
+        }
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        /*
+        -Zastanowic sie nad ustawianiem wielkosci kopca
+        -Usuwanie z kopca
+        -Zapis i odczyt do pliku
+        -Wyszukanie dowolnego elementu kopca
+         */
 
     }
 
