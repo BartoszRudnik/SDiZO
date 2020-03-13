5167c50f7a70cee7a0d685fc6ca57728fcbcbfa5
Bartosz Rudnik <rudnik49@gmail.com>
2020-03-13 15:02:34 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+    /*
+        Zapisywanie i wczytywanie do pliku (lista)
+        Wyszukiwanie elementu z listu
+        Dodawanie elementu listy na dowolne miejsce
 
+     */
 
     }
 
