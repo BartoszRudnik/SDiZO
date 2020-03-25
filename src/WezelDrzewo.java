d4384df1f087caec18df1b786ef8655623a21857
Bartosz Rudnik <rudnik49@gmail.com>
2020-03-25 14:04:01 +0100

    wezel drzewa czerwono-czarnego

--- src/WezelDrzewo.java
+++ src/WezelDrzewo.java
@@ -1,4 +1,63 @@
 public class WezelDrzewo {
 
+    private int wartosc;
+    WezelDrzewo ojciec;
+    WezelDrzewo lSyn;
+    WezelDrzewo pSyn;
+    boolean color;
+
+    public WezelDrzewo() {
+
+        wartosc = 0;
+        ojciec = null;
+        lSyn = null;
+        pSyn = null;
+
+    }
+
+    public WezelDrzewo(int wartosc) {
+
+        this.wartosc = wartosc;
+        ojciec = null;
+        lSyn = null;
+        pSyn = null;
+
+    }
+
+    public void setWartosc(int wartosc) {
+        this.wartosc = wartosc;
+    }
+
+    public void setOjciec(WezelDrzewo ojciec) {
+        this.ojciec = ojciec;
+    }
+
+    public void setlSyn(WezelDrzewo lSyn) {
+        this.lSyn = lSyn;
+    }
+
+    public void setpSyn(WezelDrzewo pSyn) {
+        this.pSyn = pSyn;
+    }
+
+    public void setColor(Boolean color) {
+        this.color = color;
+    }
+
+    public int getWartosc() {
+        return wartosc;
+    }
+
+    public WezelDrzewo getOjciec() {
+        return ojciec;
+    }
+
+    public WezelDrzewo getlSyn() {
+        return lSyn;
+    }
+
+    public WezelDrzewo getpSyn() {
+        return pSyn;
+    }
 
 }
